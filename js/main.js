d93ba438c8b472b7ec4732184e6a92b32484c7f1
Chris Nelson <chrisnelson@Chriss-MacBook-Pro.local>
2018-03-24 16:05:08 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("connected");
+//console.log("connected");
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+
+var ageYear = prompt( "How old are you?");
+
+var ageMonth = ageYear * 12
+var ageDays = ageYear * 365
+
+alert("Hello" + " " + firstName + " " + lastName + "," + " " + "your age in months is" + " " + ageMonth + "," + " " + "and in days it is" + " " + ageDays + "!");
